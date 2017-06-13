a2842f9aa826fac658f532e307b69a6f95732839
brayoh <bkuriakb@gmail.com>
2017-06-13 09:45:10 +0300

    merged find_prime repo into main bootcamp repo

--- find_prime.py
+++ find_prime.py
@@ -1,22 +1,24 @@
-def find_prime_number(number):
-    prime_numbers = []
-    # check if the given input is an integer
-    if isinstance(number, int):
-        # check if value is negative
-        if number < 0:
-            raise ValueError("input should be a positive number")
-        elif number == 0:
-            raise ValueError("input should be greater than 0")
-        else:
-            for num in range(0, number + 1):
-                # prime numbers should be greater than 1
-                if num > 1:
-                    for i in range(2, num):
-                        if num % i == 0:
-                            break # skip optional else
-                    else:
-                        prime_numbers.append(num) # append the prime number to our list
-            return prime_numbers
+import unittest
+from find_prime import find_prime_number
 
-    else:
-        raise TypeError("input should be of type integer")
+class TestGetPrimeNumbers(unittest.TestCase):
+
+    def test_returns_correct_output(self):
+        result = find_prime_number(11)
+        self.assertEquals(result, [2, 3, 5, 7, 11])
+
+    def test_returns_error_if_number_is_zero(self):
+        with self.assertRaises(ValueError):
+            find_prime_number(0)
+
+    def test_returns_error_if_number_is_negative(self):
+        with self.assertRaises(ValueError):
+            find_prime_number(-1)
+
+    def test_it_should_accept_integers_only(self):
+        with self.assertRaises(TypeError):
+            find_prime_number("String")
+
+    def test_it_accepts_whole_numbers_only(self):
+        with self.assertRaises(TypeError):
+            find_prime_number(1.5)
