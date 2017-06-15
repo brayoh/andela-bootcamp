7fcf467c555efca7b9c59910db8b262c6e0dfde4
brayoh <bkuriakb@gmail.com>
2017-06-15 14:53:41 +0300

    bug fix for binary search

--- Day 4/binary_search/binary_search.py
+++ Day 4/binary_search/binary_search.py
@@ -1,4 +1,5 @@
-class BinarySearch(object):
+
+class BinarySearch(list):
     """ BinarySearch
         takes 2 paramaters:
         a = length of list to generate
@@ -8,27 +9,29 @@ class BinarySearch(object):
         result = []
         for num in self.get_item_list(length, step_diff):
             result.append(num)
+            self.append(num)
 
-        self.list = sorted(result)
-        self.length = len(result);
+        self.list = result
+
+    def __getitem__(self, key):
+        return list.__getitem__(self, key)
 
     @property
     def length(self):
-        return self.length
+        return len(self.list)
 
     # this method is a generator function,
     # its an iterator that generates values on the fly but doesn't keep them in memory
     # this will help improve the effiency of our program given a bigger input
 
     def get_item_list(self, item_range, step_diff):
-        current = 1
+        current = step_diff
         for i in range(item_range):
             yield current
 
             # assuming diff is 2, since a is 1 the yielded value will be 2 so you add 2 + 2
             # now a will be 4 then you call yield again and a is now equal to 4 so we add 4 + 2
             # now a = 6 and so on.....
-
             current = step_diff + current
 
     # this method takes number you want to find as a paramater
@@ -36,21 +39,39 @@ class BinarySearch(object):
 
     def search(self, number):
         numbers = self.list
-        result = {'count':0}
+        result = {'count':0, 'index': -1}
         first_index = 0
         last_index = len(numbers) - 1 # list indexes start from 0 so we minus 1 to cover for that
         found = False
-        find_middle = self.find_middle(numbers)
 
-        while(first_index <= last_index and not found):
-            mid = (first_index + last_index)//2
-            result['count'] = result['count']  + 1
-            if numbers[mid] == number:
-                found = True
-                result['index'] = mid
-            else:
-            	if number < numbers[mid]: # lower half
-            		last_index = mid - 1
-            	else:  # upper half
-            		first_index = mid + 1
-        return result
+        if number == numbers[first_index]:
+            result['index'] = first_index
+            return result
+        elif number == numbers[last_index]:
+            result['index'] = last_index
+            return result
+        else:
+            while first_index <= last_index and not found:
+                mid = (first_index + last_index)//2
+                result['count'] = result['count'] + 1
+
+                if numbers[mid] == number:
+                    found = True
+                    result['index'] = mid
+                else:
+                    if number < numbers[mid]: # lower half
+                        numbers = numbers[first_index: mid] # remove the upper part
+                        mid = (len(numbers)//2) - 1
+                        if numbers[mid] == number:
+                            found = True
+                            result['index'] = self.list.index(number)
+
+                        last_index = len(numbers) - 1
+                    else:  # upper half
+                        first_index = mid +1
+
+            try:
+                self.list.index(number)
+                return result
+            except ValueError:
+                return { 'count': result['count'], 'index': -1 }
